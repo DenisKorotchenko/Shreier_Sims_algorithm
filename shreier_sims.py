fd9be542bbdc028de473886da8d3675bedf810de
DenisKorotchenko <dzd1024ozd@gmail.com>
2020-05-17 15:10:46 +0300

    Add ShreierTree class

--- shreier_sims.py
+++ shreier_sims.py
@@ -21,4 +21,34 @@ class Permutation:
         return tmp
 
 
-#=================================
+# =================================
+# дерево Шрайера
+class ShreierTree:
+    class Node:
+        def __init__(self, parent, edge, permutation_to_root):
+            self.parent = parent
+            self.edge = edge
+            self.permutation_to_root
+            pass
+
+    # вход: x - вершина, которую хотим поместить в корень дерева, generations - набор образующих
+    def __init__(self, x, generations):
+        self.generations = set(generations)
+        self.graph = {x: ShreierTree.Node()}
+        self.__build_tree(x)
+
+    # функция для построения дерева через dfs, не должна вызываться из вне, только из init-а
+    def __build_tree(self, x):
+        bfs = [x]
+        ind = 0
+        while ind < len(bfs):
+            x = bfs[ind]
+            # проходим по всем образующим, пытаясь применить их к нашей текущей x
+            for gen in self.generations:
+                # t - результат применения gen к x (gen(x))
+                t = gen[x]
+                # проверяем, что наш элемент ещё не лежит в дереве (что мы ещё его не просматривали)
+                if self.graph[t] is None:
+
+                    bfs.append(t)
+            ind += 1
